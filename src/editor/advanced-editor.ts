ac144de2426550199a3e289175a1d7cc8f0489da
Johan Frick <johan.frick@gmail.com>
2024-04-08 21:15:44 +0200

    feature: make it possible to don't switch player when grouping

--- src/editor/advanced-editor.ts
+++ src/editor/advanced-editor.ts
@@ -93,6 +93,10 @@ export const ADVANCED_SCHEMA = [
     selector: { boolean: {} },
   },
   {
+    name: 'dontSwitchPlayerWhenGrouping',
+    selector: { boolean: {} },
+  },
+  {
     type: 'string',
     help: 'Override default fallback artwork image if artwork is missing for the currently selected media',
     name: 'fallbackArtwork',
