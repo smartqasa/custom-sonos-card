4ddf7ae904e9b0ef74d55213076f7014941ca546
Johan Frick <johan.frick@gmail.com>
2024-10-16 21:55:25 +0200

    uth: cleanup copy script

--- copy-to-maxi-media-player.sh
+++ copy-to-maxi-media-player.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rsync -av --exclude='copy-to-maxi-media-player.sh' --exclude='*.DS_Store' --exclude='coverage' --exclude='node_modules' --exclude='.git' --exclude='.idea' --exclude='dist' . ../maxi-media-player
+rsync --exclude='copy-to-maxi-media-player.sh' --exclude='*.DS_Store' --exclude='coverage' --exclude='node_modules' --exclude='.git' --exclude='.idea' --exclude='dist' . ../maxi-media-player
 
 find ../maxi-media-player -name "*" \
   -not -path "*/.idea/*" \
@@ -7,14 +7,14 @@ find ../maxi-media-player -name "*" \
   -not -path "*/node_modules/*" \
   -not -path "*/coverage/*" -type f \
   -exec gsed -i '/\/\/#ONLY_SONOS_CARD_START/,/\/\/#ONLY_SONOS_CARD_END/d' {} \; \
-  -exec perl -i -pe 's/.*#ONLY_SONOS_CARD.*\n//' {} \; \
-  -exec sed -i '' -e "s/'sonos-card'/'maxi-media-player'/g" {} \; \
-  -exec sed -i '' -e "s/'Sonos'/'Maxi Media Player'/g" {} \; \
-  -exec sed -i '' -e 's/custom-sonos-card/maxi-media-player/g' {} \; \
-  -exec sed -i '' -e 's/custom:sonos-card/custom:maxi-media-player/g' {} \; \
-  -exec sed -i '' -e 's/raw\/master/raw\/main/g' {} \; \
-  -exec sed -i '' -e 's/sonos-card-/mxmp-/g' {} \; \
-  -exec sed -i '' -e 's/sonos-/mxmp-/g' {} \; \
-  -exec sed -i '' -e 's/sonos-card-dispatch/mxmp-dispatch/g' {} \; \
-  -exec sed -i '' -e 's/Sonos Card/Maxi Media Player/g' {} \; \
-  -exec sed -i '' -e 's/Media player for your Sonos speakers/Media card for Home Assistant UI with a focus on managing multiple media players/g' {} \;
+  -exec gsed -i 's/.*#ONLY_SONOS_CARD.*//g' {} \; \
+  -exec gsed -i "s/'sonos-card'/'maxi-media-player'/g" {} \; \
+  -exec gsed -i "s/'Sonos'/'Maxi Media Player'/g" {} \; \
+  -exec gsed -i 's/custom-sonos-card/maxi-media-player/g' {} \; \
+  -exec gsed -i 's/custom:sonos-card/custom:maxi-media-player/g' {} \; \
+  -exec gsed -i 's/raw\/master/raw\/main/g' {} \; \
+  -exec gsed -i 's/sonos-card-/mxmp-/g' {} \; \
+  -exec gsed -i 's/sonos-/mxmp-/g' {} \; \
+  -exec gsed -i 's/sonos-card-dispatch/mxmp-dispatch/g' {} \; \
+  -exec gsed -i 's/Sonos Card/Maxi Media Player/g' {} \; \
+  -exec gsed -i 's/Media player for your Sonos speakers/Media card for Home Assistant UI with a focus on managing multiple media players/g' {} \;
