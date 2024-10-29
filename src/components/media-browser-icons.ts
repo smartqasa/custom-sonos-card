98301be95d8e489213fbc1a7788d1d1ed4486170
Tom Bruno <tom.bruno@smartqasa.com>
2024-10-29 09:30:54 -0400

    Adjust button size and spacing in media browser icons

--- src/components/media-browser-icons.ts
+++ src/components/media-browser-icons.ts
@@ -68,10 +68,6 @@ export class MediaBrowserIcons extends LitElement {
           padding: 0 1rem;
         }
 
-        .button {
-          --control-button-padding: 0;
-        }
-
         .thumbnail {
           width: 100%;
           padding-bottom: 100%;
